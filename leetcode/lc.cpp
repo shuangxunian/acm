4838d5b173dafe46372672f693c8f57437090380
YuelinWang <836473734@qq.com>
2020-08-18 23:02:50 +0800

    read readme

--- leetcode/lc.cpp
+++ leetcode/lc.cpp
@@ -312,3 +312,22 @@ public:
         return rev;
     }
 };
+
+
+//lc 009
+//https://leetcode-cn.com/problems/palindrome-number/
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+};
--- leetcode/lc.js
+++ leetcode/lc.js
@@ -268,12 +268,68 @@ var numIdenticalPairs = function(nums) {
 };
 
 //
-var quickSort = function(arr) {　　
-    if (arr.length <= 1) { return arr; }　　
-    var pivotIndex = Math.floor(arr.length / 2);　　
-    var pivot = arr.splice(pivotIndex, 1)[0];　　
-    var left = [];　　
-    var right = [];　　
-    for (var i = 0; i < arr.length; i++) {　　　　 if (arr[i] < pivot) {　　　　　　 left.push(arr[i]);　　　　 } else {　　　　　　 right.push(arr[i]);　　　　 }　　 }　　
+var quickSort = function(arr) {
+    if (arr.length <= 1) { return arr; }
+    var pivot = arr.splice(0, 1)[0];
+    var left = [];
+    var right = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
     return quickSort(left).concat([pivot], quickSort(right));
+};
+
+function fn() {
+    var x = true;
+    return function() {
+        console.log(x ? 1 : 2);
+        x = !x;
+    }
+}
+var a = fn();
+
+
+//lc 009
+//https://leetcode-cn.com/problems/palindrome-number/
+var isPalindrome = function(x) {
+    return x.toString() == x.toString().split("").reverse().join("");
+}
+
+//lc 013
+//https://leetcode-cn.com/problems/roman-to-integer/
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const map = {
+        I: 1,
+        IV: 4,
+        V: 5,
+        IX: 9,
+        X: 10,
+        XL: 40,
+        L: 50,
+        XC: 90,
+        C: 100,
+        CD: 400,
+        D: 500,
+        CM: 900,
+        M: 1000
+    };
+    let ans = 0;
+    for (let i = 0; i < s.length;) {
+        if (i + 1 < s.length && map[s.substring(i, i + 2)]) {
+            ans += map[s.substring(i, i + 2)];
+            i += 2;
+        } else {
+            ans += map[s.substring(i, i + 1)];
+            i++;
+        }
+    }
+    return ans;
 };
