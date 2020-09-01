750678088d2e09a6450e1fe6cff31050b498452a
YuelinWang <836473734@qq.com>
2020-09-01 20:45:51 +0800

    read

--- leetcode/lc.js
+++ leetcode/lc.js
@@ -743,3 +743,18 @@ var countAndSay = function(n) {
     }
     return prev
 };
+
+//lc 58
+//https://leetcode-cn.com/problems/length-of-last-word/
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let end = s.length - 1;
+    while (end >= 0 && s[end] == ' ') end--;
+    if (end < 0) return 0;
+    let start = end;
+    while (start >= 0 && s[start] != ' ') start--;
+    return end - start;
+};
