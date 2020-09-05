0322e1121a6f5c281ab9abf5ad8acf23aad3e3eb
YuelinWang <836473734@qq.com>
2020-09-05 20:55:54 +0800

    read

--- leetcode/lc.js
+++ leetcode/lc.js
@@ -968,3 +968,50 @@ var getPermutation = function(n, k) {
     }
     return ans;
 };
+
+//lc 83
+//https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    let ans = head;
+    while (ans != null && ans.next != null) {
+        if (ans.val == ans.next.val) ans.next = ans.next.next;
+        else ans = ans.next;
+    }
+    return head;
+};
+
+//lc 88
+//https://leetcode-cn.com/problems/merge-sorted-array/
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    let len1 = m - 1;
+    let len2 = n - 1;
+    let len = m + n - 1;
+    while (len1 >= 0 && len2 >= 0) {
+        // 注意--符号在后面，表示先进行计算再减1，这种缩写缩短了代码
+        nums1[len--] = nums1[len1] > nums2[len2] ? nums1[len1--] : nums2[len2--];
+    }
+
+    function arrayCopy(src, srcIndex, dest, destIndex, length) {
+        dest.splice(destIndex, length, ...src.slice(srcIndex, srcIndex + length));
+    }
+    // 表示将nums2数组从下标0位置开始，拷贝到nums1数组中，从下标0位置开始，长度为len2+1
+    arrayCopy(nums2, 0, nums1, 0, len2 + 1);
+};
