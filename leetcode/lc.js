b3442a80e346144977526dac822db6ab54493b3b
YuelinWang <836473734@qq.com>
2020-09-08 20:49:19 +0800

    read

--- leetcode/lc.js
+++ leetcode/lc.js
@@ -1260,3 +1260,39 @@ var getLeastNumbers = function(arr, k) {
 
     return arr.slice(0, k);
 };
+
+//lc 77
+//https://leetcode-cn.com/problems/combinations/
+//py 3
+//我不会，就是看太神奇了就给抄下来了
+/*
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        return list(itertools.combinations(range(1,n+1),k))
+*/
+var combine = function(n, k) {
+    const temp = [];
+    const ans = [];
+    // 初始化
+    // 将 temp 中 [0, k - 1] 每个位置 i 设置为 i + 1，即 [0, k - 1] 存 [1, k]
+    // 末尾加一位 n + 1 作为哨兵
+    for (let i = 1; i <= k; ++i) {
+        temp.push(i);
+    }
+    temp.push(n + 1);
+
+    let j = 0;
+    while (j < k) {
+        ans.push(temp.slice(0, k));
+        j = 0;
+        // 寻找第一个 temp[j] + 1 != temp[j + 1] 的位置 t
+        // 我们需要把 [0, t - 1] 区间内的每个位置重置成 [1, t]
+        while (j < k && temp[j] + 1 == temp[j + 1]) {
+            temp[j] = j + 1;
+            ++j;
+        }
+        // j 是第一个 temp[j] + 1 != temp[j + 1] 的位置
+        ++temp[j];
+    }
+    return ans;
+};
